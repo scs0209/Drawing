9924e2fa7a5c2028747db89704f9fcdede496c78
scs0209 <110822847+scs0209@users.noreply.github.com>
2022-09-13 00:32:22 +0900

    그림판 완성!

--- MEME-MAKER/app.js
+++ MEME-MAKER/app.js
@@ -1,6 +1,10 @@
+const saveBtn = document.getElementById("save");
+const textInput = document.getElementById("text");
+const fileInput =  document.getElementById("file");
 const modeBtn = document.getElementById("mode-btn");
 const resetBtn = document.getElementById("reset-btn");
 const eraseBtn = document.getElementById("eraser-btn");
+//forEach함수를 쓰기 위해서는 배열형태로 바꿔줘야되기 때문에 Array사용
 const colorOptions = Array.from(document.getElementsByClassName("color-option"));
 const color = document.getElementById("color");
 const lineWidth = document.getElementById("line-width");
@@ -13,7 +17,8 @@ const CANVAS_HEIGHT = 800;
 
 canvas.width = CANVAS_WIDTH;
 canvas.height = CANVAS_HEIGHT;
-ctx.lineWidth = lineWidth.value; //초기값 4로 설정(HTML)
+ctx.lineWidth = lineWidth.value; //초기값 5로 설정(HTML), 브라우저가 실행될때 딱 한번만 실행됨
+ctx.lineCap = "round";
 let isPainting = false;
 let isFilling = false;
 
@@ -79,8 +84,53 @@ function onEraseClick(){
   isFilling = false
   modeBtn.innerText = "Fill"
 }
+
+//이미지 가져오기
+function onFileChange(event){
+  const file = event.target.files[0];
+  //우리는 url을 통해서 해당 파일에 접근하려는거기 때문에 createObjectURL을 사용 => 유저가 파일을 업로드한 브라우저 안에서만 사용할 수 있는 url
+  const url = URL.createObjectURL(file);
+  const image = new Image() //html에서 <img src="">와 같은 의미
+  //위의 코드는 document.createElement("image")와 같은 코드
+  image.src = url;
+  //url로 이미지를 가져와서 화면에 그림
+  image.onload = function(){
+    ctx.drawImage(image, 0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+    fileInput.value = null;
+  }
+}
+
+//더블클릭하여 텍스트를 canvas안에 집어 넣어주기
+function onDoubleClick(event){
+  const text = textInput.value;
+  if(text !== ""){
+    ctx.save();
+    ctx.lineWidth = 1;
+    ctx.font = "68px serif";
+    ctx.fillText(text,event.offsetX, event.offsetY);
+    //offsetX, offsetY는 마우스가 클릭한 canvas 내부좌표
+    ctx.restore();
+  }
+}
+//ctx.save ~ ctx.restore사이에 있는 코드는 저장이 되지 않고 실행된 후에 기존에 있던 코드들이 다시 진행된다.
+//즉 ctx.save를 한 지점으로 다시 돌아간다는 얘기이다.
+
+
+//이미지 저장
+function onSaveClick(){
+  //캔버스에 있는 그림 데이터를 url로 변환해주는 메소드
+  const url = canvas.toDataURL();
+  const a = document.createElement("a");
+  a.href = url
+  a.download = "myDrawing.png"
+  console.log(a);
+  a.click();
+}
+
+
 //click은 마우스를 눌렀다 뗐을때를 의미
 //mousedown은 마우스를 누른 채로 있을때를 의미
+canvas.addEventListener("dblclick", onDoubleClick);
 canvas.addEventListener("mousemove", onMove);
 canvas.addEventListener("mousedown", startPainting);
 canvas.addEventListener("mouseup", cancelPainting);
@@ -94,3 +144,5 @@ colorOptions.forEach((color) => color.addEventListener("click", onColorClick));
 modeBtn.addEventListener("click", onModeClick);
 resetBtn.addEventListener("click", onResetClick);
 eraseBtn.addEventListener("click", onEraseClick);
+fileInput.addEventListener("change", onFileChange);
+saveBtn.addEventListener("click", onSaveClick);
